dae5112b968bb57e09d396b618e4533bf1c04cee
Grishma Kagde <greeshma.kagde4@gmail.com>
2019-07-22 17:06:16 +0530

    Added movieXmL.class

--- XMLSxsProject/src/com/zensar/sax/parsers/MovieXmlHandler.java
+++ XMLSxsProject/src/com/zensar/sax/parsers/MovieXmlHandler.java
@@ -15,31 +15,33 @@ public class MovieXmlHandler extends DefaultHandler {
 		}
 		System.out.println();
 		
-		super.characters(arg0, arg1, arg2);
+	
 	}
 
 	@Override
 	public void endDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.endDocument();
+		
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		// TODO Auto-generated method stub
-		super.endElement(uri, localName, qName);
+		System.out.println("ENd Element"+qName);
 	}
 
 	@Override
 	public void startDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.startDocument();
+		System.out.println("Starting...");
 	}
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		// TODO Auto-generated method stub
-		super.startElement(uri, localName, qName, attributes);
+	System.out.println("StartName:" +qName);
+	
+	
 	}
 
 }
